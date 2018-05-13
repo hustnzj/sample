ab22b6ba287ba4148fd23dd059067fc2c8c85f7d
hustnzj <79649301@qq.com>
2018-05-13 07:22:37 +0000

    Finish user edit, update, index, and destroy actions

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,7 +18,9 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect('/home');
+            $word = $request->is('signup') ? '注册' : '登陆';
+            session()->flash('info', '您已'.$word.'，无需再次操作。');
+            return redirect('/');
         }
 
         return $next($request);
