460680e19b2220e4837eb044001e44a8dbc7dfd5
hustnzj <79649301@qq.com>
2018-05-14 01:58:16 +0000

    Add account activations

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -19,6 +19,7 @@ class UsersTableSeeder extends Seeder
         $user->email = '79649301@qq.com';
         $user->password = bcrypt('111111');
         $user->is_admin = true;
+        $user->activated = true;
         $user->save();
     }
 }
