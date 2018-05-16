99eed1d10a896efa4d5e5b940e7313a4c6973450
hustnzj <79649301@qq.com>
2018-05-16 13:34:24 +0000

    Add user statuses

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <title>@yield('title', 'Sample') - Laravel elementary course</title>
-    <link rel="stylesheet" href="/css/bootstrap.css">
+    {{--<link rel="stylesheet" href="/css/bootstrap.css">--}}
     <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
